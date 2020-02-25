f3497e7f23ebab02b6b7478a34195550ab185cac
dexen deVries <dexen.devries@gmail.com>
2020-02-25 01:42:53 +0100

    approximate Git's root dir detection algorithm

--- src/Repository.php
+++ src/Repository.php
@@ -16,14 +16,40 @@ class Repository
 		return $this->Config;
 	}
 
+	protected static
+	function _checkValidRepository(string $pathname) : ?string
+	{
+		if (!is_dir($pathname))
+			return sprintf('not a git repository: not a directory: "%s"', $pathname);
+		if (!file_exists($pathname .'/HEAD'))
+			return sprintf('not a git repository: missing "%s"', 'HEAD');
+		if (!is_dir($pathname .'/objects'))
+			return sprintf('not a git repository: missing "%s"', 'objects');
+		return null;
+	}
+
 	static
 	function checkValidRepository(string $pathname)
 	{
-		if (!is_dir($pathname))
-			throw new Exception(sprintf('not a git repository: not a directory: "%s"', $pathname));
-		if (!file_exists($pathname .'/HEAD'))
-			throw new Exception(sprintf('not a git repository: missing "%s"', 'HEAD'));
-		if (!is_dir($pathname .'/objects'))
-			throw new Exception(sprintf('not a git repository: missing "%s"', 'objects'));
+		$error = static::_checkValidRepository($pathname);
+		if ($error !== null)
+			throw new Exception($error);
+	}
+
+	static
+	function fromCwd()
+	{
+		$dir = getcwd();
+		$a = explode('/', $dir);
+		while ($a) {
+			$candidate = implode('/', $a);
+				# usual scenario
+			if (static::_checkValidRepository($candidate .'/.git') === null)
+				return new static($candidate);
+				# bare repo
+			if (static::_checkValidRepository($candidate) === null)
+				return new static(null, $candidate);
+			array_pop($a); }
+		throw new Exception(sprintf('not a git repository (or any of the parent directories): "%s"', $dir));
 	}
 }
